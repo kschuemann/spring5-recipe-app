2a042b46f9c4c2d19cf9672eab38f11b21de35a8
kschuemann <kolja.schuemann@gmail.com>
2020-08-30 15:18:34 +0200

    added test for show todo

--- src/test/java/guru/springframework/controllers/TodosControllerTest.java
+++ src/test/java/guru/springframework/controllers/TodosControllerTest.java
@@ -1,36 +1,60 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Todo;
+import guru.springframework.services.RecipeService;
+import guru.springframework.services.TodoService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 class TodosControllerTest {
 
-    @Test
-    void standardPostMethod() {
+    @Mock
+    TodoService todoService;
+
+    TodosController todosController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        todosController = new TodosController(todoService);
+        mockMvc = MockMvcBuilders.standaloneSetup(todosController).build();
     }
 
     @Test
-    void listRecipes() {
+    void listTodos() {
     }
 
     @Test
-    void showById() {
+    void showById() throws Exception {
+
+        Todo todo = new Todo();
+        todo.setId(1L);
+        when(todoService.findById(anyLong())).thenReturn(todo);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/todos/show/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("todos/show"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("todo"));
+
     }
 
     @Test
-    void newRecipe() {
+    void newTodo() {
     }
 
     @Test
-    void processCreationForm() {
-    }
-
-    @Test
-    void testNewRecipe() {
-    }
-
-    @Test
-    void editRecipe() {
+    void testNewTodo() {
     }
 }
